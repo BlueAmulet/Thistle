edbd602069c59afbe35536bf9b5ec5cd6e6fa3e0
gamax92 <gamax92@aol.com>
2017-04-02 14:56:57 -0600

    Increment version for release
    Pretty big changes here, so set to 1.1.0 instead of 1.0.5

--- src/main/java/gamax92/thistle/Thistle.java
+++ src/main/java/gamax92/thistle/Thistle.java
@@ -19,7 +19,7 @@ import net.minecraftforge.common.config.Configuration;
 public class Thistle {
 	public static final String MODID = "thistle";
 	public static final String NAME = "Thistle Computer";
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.1.0";
 
 	@Mod.Instance
 	public static Thistle instance;
